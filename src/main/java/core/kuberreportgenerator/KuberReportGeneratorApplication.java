be79f01ff1b8a8c05e0f8ed6cf4e0bd088a719f4
Pasha Fentisov <pasha16.ua@gmail.com>
2025-01-09 20:44:06 +0200

    Initial commit

---
+++ src/main/java/core/kuberreportgenerator/KuberReportGeneratorApplication.java
@@ -0,0 +1,13 @@
+package core.kuberreportgenerator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KuberReportGeneratorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KuberReportGeneratorApplication.class, args);
+    }
+
+}
